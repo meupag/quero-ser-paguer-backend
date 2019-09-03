b96f760ce96cb8f383008203010f2ccbe9cfac56
Gustavo Camargo <gutoca77@gmail.com>
2019-09-03 17:49:42 -0300

    Adjust error handler

--- clients-api/api/helpers/serverErrorHandler.js
+++ clients-api/api/helpers/serverErrorHandler.js
@@ -2,8 +2,8 @@ module.exports = function(res, error) {
     res.status(500);
     let message = "Unhandled request error";
     if(process.env.DEBUG){
-        res.json({message: message.concat(":" + JSON.stringify(error))});
+        res.json([{message: message.concat(":" + JSON.stringify(error)), type: "UNHANDLED_REQUEST_ERROR" }]);
     }
 
-    res.json({message: message});
+    res.json([{message: message, type: "UNHANDLED_REQUEST_ERROR" }]);
 }
