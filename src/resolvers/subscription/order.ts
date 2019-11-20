7d8a4b3a89b4db7cfc623c447270aa69f662fa73
Cassiano Kunsch <cassiano.kunsch@outlook.com>
2019-11-20 16:03:52 -0300

    Adicionando os arquivos no fork

--- src/resolvers/subscription/order.ts
+++ src/resolvers/subscription/order.ts
@@ -2,11 +2,13 @@ import { Context } from '../../utils';
 import { OrderSubscriptionPayloadSubscription } from '../../generated/prisma-client';
 
 const newSubsOrder = (_parent, _args, ctx: Context): OrderSubscriptionPayloadSubscription => {
+  // eslint-disable-next-line @typescript-eslint/camelcase
   return ctx.prisma.$subscribe.order({ mutation_in: 'CREATED' }).node();
 };
 
 const newOrder = {
   subscribe: newSubsOrder,
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   resolve: payload => {
     return payload;
   },
